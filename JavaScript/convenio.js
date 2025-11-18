ff548f3cc66b5d440193c465c804cc7cc8fdbf6b
Mangel2905 <angelcipriano1720@gmail.com>
2025-11-17 20:09:08 -0500

    Añadi boton mostrar convenios que muestra una tabla

--- JavaScript/convenio.js
+++ JavaScript/convenio.js
@@ -1,3 +1,76 @@
+var instituciones = [
+    {
+        nombre: "Diris Lima Norte",
+        tasa: "11%",
+        imagen: "../../imagenes/DirisNorte.jpg"
+    },
+    {
+        nombre: "Diris Lima Centro",
+        tasa: "10.99%",
+        imagen: "../../imagenes/DirisCentro.jpg"
+    },
+    {
+        nombre: "Diris Lima Este",
+        tasa: "10.99%",
+        imagen: "../../imagenes/DirisEste.jpg"
+    },
+    {
+        nombre: "Poder Judicial",
+        tasa: "11.18%",
+        imagen: "../../imagenes/PoderJudicial.jpg"
+    },
+    {
+        nombre: "Policia Nacional del Peru",
+        tasa: "10.99%",
+        imagen: "../../imagenes/PNP.jpg"
+    },
+    {
+        nombre: "Ejercito Peruano",
+        tasa: "11.6%",
+        imagen: "../../imagenes/EjercitoPeruano.jpg"
+    },
+    {
+        nombre: "SENASA",
+        tasa: "11.35%",
+        imagen: "../../imagenes/SENASA.jpg"
+    },
+];
+
+// Detectar el botón usando su class
+var boton = document.querySelector(".MostrarConvenio");
+
+boton.onclick = function () {
+    mostrarTabla();
+};
+
+// Función que arma la tabla
+function mostrarTabla() {
+    var contenedor = document.getElementById("contenedorTabla");
+    contenedor.style.display = "block";
+    var tabla = `
+        <table>
+            <tr>
+                <th>Institución</th>
+                <th>Tasa de Interés</th>
+                <th>Logo</th>
+            </tr>
+    `;
+
+    for (var i = 0; i < instituciones.length; i++) {
+        tabla += `
+            <tr>
+                <td>${instituciones[i].nombre}</td>
+                <td>${instituciones[i].tasa}</td>
+                <td><img src="${instituciones[i].imagen}" alt="logo"></td>
+            </tr>
+        `;
+    }
+
+    tabla += "</table>";
+
+    contenedor.innerHTML = tabla;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 
     const slides = document.querySelectorAll(".slide");
